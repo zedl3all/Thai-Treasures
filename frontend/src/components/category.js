cc38339be7320d403d28866d3fe2de1aa984797c
zedl3all <sanguanwongtanapat@gmail.com>
2026-03-01 05:08:50 +0700

    Add: Create categories constant for product classification

---
+++ frontend/src/components/category.js
@@ -0,0 +1,15 @@
+export const CATEGORIES = [
+    "Accessory",
+    "Amulet",
+    "Art",
+    "Ceramics",
+    "Fashion",
+    "Food",
+    "Health",
+    "Home Decor",
+    "Jewelry",
+    "Pottery",
+    "Souvenir",
+    "Spa",
+    "Textiles"
+];
